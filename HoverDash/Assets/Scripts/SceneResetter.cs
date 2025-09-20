7646323862f35091b608223aa987d1a11d3fedff
Ryan Vassallo <106540868+Ryanv14@users.noreply.github.com>
2025-09-20 18:26:59 +0200

    Buttons + Zen Level
    Updated button colours to match the light blue aesthetics and also updated the layout of Zen Level

--- HoverDash/Assets/Scripts/SceneResetter.cs
+++ HoverDash/Assets/Scripts/SceneResetter.cs
@@ -6,9 +6,7 @@ public class SceneResetter : MonoBehaviour
 {
     private void Update()
     {
-        // Allow quick restart by pressing R
-        if (Input.GetKeyDown(KeyCode.R))
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+      
     }
 }
 
